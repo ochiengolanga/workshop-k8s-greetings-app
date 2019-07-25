9f843c2cbfbe1115d3385a7a316f575d056b20f7
Ochieng Olanga <ochiengolanga@gmail.com>
2019-07-25 15:00:24 +0200

    Added jaegar to dashboard script

--- manifest/istio/dashboards.sh
+++ manifest/istio/dashboards.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
-kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090 &
+
+# Prometheus
+kubectl port-forward -n istio-system $(kubectl -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090 &
+
+# Grafana
 kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}') 3000:3000 &
-kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=zipkin -o jsonpath='{.items[0].metadata.name}') 15032:9411
+
+# Zipkin
+#kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=zipkin -o jsonpath='{.items[0].metadata.name}') 15032:9411
+
+# Jaeger
+kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=jaeger -o jsonpath='{.items[0].metadata.name}') 16686:16686 &
+  
+# Kiali
+#kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=kiali -o jsonpath='{.items[0].metadata.name}') 20001:20001 &
